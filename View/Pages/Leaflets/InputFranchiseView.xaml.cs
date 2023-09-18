41537504bc614da1fedcad4aab450de125588ddc
lewis <lewis@DESKTOP-31LSRVA>
2023-09-18 14:05:24 +0800

    Added TableView and InputFranchiseView

---
+++ View/Pages/Leaflets/InputFranchiseView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SPTC_APP.View.Pages.Leaflets
+{
+    /// <summary>
+    /// Interaction logic for TupleView.xaml
+    /// </summary>
+    public partial class InputFranchiseView : Window
+    {
+        public InputFranchiseView()
+        {
+            InitializeComponent();
+        }
+    }
+}

b577b7acdf21adbd49b9cb1b9b0254a35838ddf8
lewis <lewis@DESKTOP-31LSRVA>
2023-09-27 19:50:07 +0800

    Added JohnRey code, Change NoBorder to Controlled Style button,

--- View/Pages/Leaflets/FranchiseInformationView.xaml.cs
+++ View/Pages/Leaflets/FranchiseInformationView.xaml.cs
@@ -2,6 +2,7 @@
 using SPTC_APP.Objects;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SPTC_APP.View.Pages.Leaflets
 {
@@ -10,6 +11,7 @@ namespace SPTC_APP.View.Pages.Leaflets
     /// </summary>
     public partial class FranchiseInformationView : Window
     {
+        private Button selectedButton = null;
         public FranchiseInformationView()
         {
             InitializeComponent();
@@ -48,5 +50,65 @@ namespace SPTC_APP.View.Pages.Leaflets
 
             }
         }
+
+        private void ClickColorControl(Button button)
+        {
+            if (selectedButton != null)
+            {
+                selectedButton.Background = Brushes.White;
+            }
+
+            button.Background = Brushes.Yellow;
+            selectedButton = button;
+        }
+
+        private void HandleButtonClick(string moduleName)
+        {
+            ModuleGrid.Children.Clear();
+            ModuleGrid.Children.Add((new Modules(moduleName)).Fetch());
+        }
+
+        private void btnHistory_Click(object sender, RoutedEventArgs e)
+        {
+            HandleButtonClick(Modules.HISTORY);
+            ClickColorControl(sender as Button);
+        }
+
+        private void btnCoding_Click(object sender, RoutedEventArgs e)
+        {
+            HandleButtonClick(Modules.CODING);
+            ClickColorControl(sender as Button);
+        }
+
+        private void btnViolation_Click(object sender, RoutedEventArgs e)
+        {
+            HandleButtonClick(Modules.VIOLATION);
+            ClickColorControl(sender as Button);
+        }
+
+        private void btnShareCapital_Click(object sender, RoutedEventArgs e)
+        {
+            HandleButtonClick(Modules.SHARECAPITAL);
+            ClickColorControl(sender as Button);
+        }
+
+        private void btnLoan_Click(object sender, RoutedEventArgs e)
+        {
+            HandleButtonClick(Modules.LOAN);
+            ClickColorControl(sender as Button);
+        }
+
+        private void btnLTLoan_Click(object sender, RoutedEventArgs e)
+        {
+            HandleButtonClick(Modules.LTLOAN);
+            ClickColorControl(sender as Button);
+        }
+
+        private void btnTransactionHistory_Click(object sender, RoutedEventArgs e)
+        {
+            HandleButtonClick(Modules.TRANSFER);
+            ClickColorControl(sender as Button);
+        }
+
     }
 }

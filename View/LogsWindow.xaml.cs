04404d33d3b3f47ab03cd6bf9179b4f98472d1ce
lewis <lewis@DESKTOP-31LSRVA>
2023-09-28 13:28:58 +0800

    Added LogsWindow, to view local logs

--- View/LogsWindow.xaml.cs
+++ View/LogsWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace SPTC_APP.View
     /// </summary>
     public partial class LogsWindow : Window
     {
+
         public LogsWindow()
         {
             InitializeComponent();
@@ -33,11 +34,12 @@ namespace SPTC_APP.View
         private void ReloadLog()
         {
             string path = AppState.LOGS;
-
+            List<LogEntry> logEntries = new List<LogEntry>();
             try
             {
                 string[] lines = File.ReadAllLines(path);
-                List<LogEntry> logEntries = new List<LogEntry>();
+
+                string tmpDay = "";
                 foreach (string line in lines)
                 {
                     string[] columns = line.Split(new string[] { " :: " }, StringSplitOptions.None);
@@ -45,16 +47,17 @@ namespace SPTC_APP.View
                     if (columns.Length >= 3)
                     {
                         string[] dateParts = columns[0].Split(' ');
-
                         if (dateParts.Length >= 2)
                         {
                             logEntries.Add(new LogEntry
                             {
-                                DayOfTheWeek = dateParts[0],
-                                DateTime = dateParts[1] + " " + dateParts[2],
-                                LogType = columns[1],
+                                Day = (tmpDay != dateParts[0] + dateParts[1] || !(cbUniqueTime.IsChecked??false))?dateParts[0]  : "",
+                                Date = (tmpDay != dateParts[0] + dateParts[1] || !(cbUniqueTime.IsChecked ?? false)) ? dateParts[1] : "",
+                                Time = dateParts[2],
+                                Type = columns[1],
                                 Message = columns[2]
                             });
+                            tmpDay = dateParts[0] + dateParts[1];
                         }
                     }
                 }
@@ -66,7 +69,18 @@ namespace SPTC_APP.View
                 ControlWindow.Show("Error: ", ex.Message, Icons.ERROR);
             }
         }
-
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            dgLogs.Items.Filter = (item) =>
+            {
+                if (item is LogEntry)
+                {
+                    LogEntry typedItem = (LogEntry)item;
+                    return typedItem.Search(tbSearch.Text.ToLower(), cbDay.IsChecked ?? false, cbDate.IsChecked ?? false, cbTime.IsChecked ?? false, cbType.IsChecked ?? false, cbMessage.IsChecked ?? false);
+                }
+                return false;
+            };
+        }
 
 
         private void btnReload_Click(object sender, RoutedEventArgs e)
@@ -75,10 +89,22 @@ namespace SPTC_APP.View
         }
         public class LogEntry
         {
-            public string DayOfTheWeek { get; set; }
-            public string DateTime { get; set; }
-            public string LogType { get; set; }
+            public string Day { get; set; }
+            public string Date { get; set; }
+            public string Time { get; set; }
+            public string Type { get; set; }
             public string Message { get; set; }
+
+            public bool Search(string search, bool searchDay, bool searchDate, bool searchTime, bool searchType, bool searchMessage)
+            {
+                if (searchDay && Day.ToLower().Contains(search)) return true;
+                if (searchDate && Date.ToLower().Contains(search)) return true;
+                if (searchTime && Time.ToLower().Contains(search)) return true;
+                if (searchType && Type.ToLower().Contains(search)) return true;
+                if (searchMessage && Message.ToLower().Contains(search)) return true;
+
+                return false;
+            }
         }
 
     }

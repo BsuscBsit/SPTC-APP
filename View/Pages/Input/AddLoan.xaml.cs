d16ed625c5c971fb4a69a69271d1805f85db952c
Lewis Daveriel Masallo <lewismasallo@outlook.com>
2023-10-25 07:41:05 +0800

    add Loan Calculations

--- View/Pages/Input/AddLoan.xaml.cs
+++ View/Pages/Input/AddLoan.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SPTC_APP.Objects;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,19 +21,48 @@ namespace SPTC_APP.View.Pages.Input
     /// </summary>
     public partial class AddLoan : Window
     {
-        public AddLoan()
+        Franchise franchise;
+        public AddLoan(Franchise franchise)
         {
             InitializeComponent();
+            ContentRendered += (sender, e) => { AppState.WindowsCounter(true, sender); };
+            Closed += (sender, e) => { AppState.WindowsCounter(false, sender); };
+            AppState.mainwindow?.Hide();
+            this.franchise = franchise;
+            dpBdate.DisplayDate = DateTime.Now;
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            AppState.mainwindow?.Show();
+            base.OnClosing(e);
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-
+            if (ControlWindow.ShowDialogStatic("Adding new record", "Confirm?", Icons.NOTIFY))
+            {
+                PaymentDetails<Ledger.Loan> capital = new PaymentDetails<Ledger.Loan>();
+                Ledger.Loan share = new Ledger.Loan();
+                if (franchise.GetShareCapitals()?.FirstOrDefault() != null)
+                {
+                    share = franchise.GetLoans().FirstOrDefault();
+                    capital.WriteInto(share, false, false, dpBdate.DisplayDate, tboxRefNo.Text, Double.Parse(tboxAmount.Text), 0, "", share.amount - Double.Parse(tboxAmount.Text));
+                    share.amount = share.amount + Double.Parse(tboxAmount.Text);
+                    share.Save();
+                } else
+                {
+                    share.WriteInto(franchise.id, DateTime.Now, Double.Parse(tboxAmount.Text), "", Double.Parse(tboxInterest.Text), 0, 0);
+                    share.Save();
+                }
+                
+                capital.Save();
+                this.Close();
+            }
         }
     }
 }

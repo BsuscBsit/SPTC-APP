5f6d807eb6d560116c1bd2a747fcb51cf2a8e3c9
Lewis Daveriel Masallo <lewismasallo@outlook.com>
2023-11-21 18:53:05 +0800

    Added report xaml

--- View/Pages/Output/Reports.xaml.cs
+++ View/Pages/Output/Reports.xaml.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.IO;
+using System.Printing;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+using Image = System.Windows.Controls.Image;
 
 namespace SPTC_APP.View.Pages.Output
 {
@@ -19,9 +16,42 @@ namespace SPTC_APP.View.Pages.Output
     /// </summary>
     public partial class Reports : Window
     {
-        public Reports()
+        private string filename;
+        public Reports(string filename)
         {
             InitializeComponent();
+            this.filename = filename;
+        }
+        public void StartPrint()
+        {
+            string outputPath = AppState.OUTPUT_PATH + filename + "_page.xps";
+
+            if (!Directory.Exists(AppState.OUTPUT_PATH))
+            {
+                Directory.CreateDirectory(AppState.OUTPUT_PATH);
+            }
+
+            using (XpsDocument xpsDoc = new XpsDocument(outputPath, FileAccess.Write))
+            {
+                XpsDocumentWriter xpsWriter = XpsDocument.CreateXpsDocumentWriter(xpsDoc);
+
+                PrintTicket printTicket = new PrintTicket(); 
+                xpsWriter.Write(reportpage, printTicket);
+            }
+
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                XpsDocument xpsDocToPrint = new XpsDocument(outputPath, FileAccess.Read);
+                FixedDocumentSequence fixedDocumentSequence = xpsDocToPrint.GetFixedDocumentSequence();
+
+                // Get a paginator for the document sequence
+                DocumentPaginator paginator = fixedDocumentSequence.DocumentPaginator;
+
+                // Print the paginator
+                printDialog.PrintDocument(paginator, "Printing XPS Document");
+                xpsDocToPrint.Close();
+            }
         }
     }
 }
